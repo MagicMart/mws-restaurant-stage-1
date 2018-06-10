c6af76085caaf269277360bddf9adff50cc1458f
Martin Tudor <martcan27@gmail.com>
2018-06-10 21:44:07 +0100

    Add more files to service worker cache

--- sw/sw2.js
+++ sw/sw2.js
@@ -1,15 +1,12 @@
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('static-v1').then((cache) =>{
+        caches.open('static-v2').then((cache) => {
             return cache.addAll(
                 ['/',
-                 '/index.html',
-                 '/restaurant.html',
-                '/css/styles.css',
-                '/js/dbhelper.js',
-                '/js/restaurant_info.js',
-                '/data/restaurants.json',
-                '/js/main.js']
+                    '/restaurant.html',
+                    '/css/styles.css',
+                    '/js/dbhelper.js'
+                ]
             );
         })
     );
