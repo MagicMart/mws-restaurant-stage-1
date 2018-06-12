2ef0fc6a17ddde6f08346ac10ead93b94be0a9c3
Martin Tudor <martcan27@gmail.com>
2018-06-12 16:21:25 +0100

    Make Reviewer's name an h3 heading
    Reviewer's name is subsection of h2 Reviews

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -121,13 +121,13 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
-  const name = document.createElement('p');
+  const name = document.createElement('h3');
   name.innerHTML = review.name;
   li.appendChild(name);
 
-  const date = document.createElement('p');
+  const date = document.createElement('span');
   date.innerHTML = review.date;
-  li.appendChild(date);
+  name.appendChild(date);
 
   const rating = document.createElement('p');
   rating.innerHTML = `Rating: ${review.rating}`;
