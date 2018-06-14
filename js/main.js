363e26229717b5960e9be7163fdec05939b06df4
Martin Tudor <martcan27@gmail.com>
2018-06-14 16:28:25 +0100

    Move service worker registration to main.js

--- js/main.js
+++ js/main.js
@@ -185,3 +185,13 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 };
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function (registration) {
+    console.log('Service worker registration succeeded:', registration);
+  }).catch(function (error) {
+    console.log('Service worker registration failed:', error);
+  });
+} else {
+  console.log('Service workers are not supported.');
+}
