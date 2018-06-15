83d6234ab7ada21da45e9051819d58e3ff7f23bc
Martin Tudor <martcan27@gmail.com>
2018-06-15 10:48:19 +0100

    Update service worker cache

--- sw.js
+++ sw.js
@@ -5,11 +5,23 @@ self.addEventListener('install', (event) => {
         caches.open('static-v1').then((cache) => {
             return cache.addAll(
                 ['https://magicmart.github.io/mws-restaurant-stage-1/',
+                'https://magicmart.github.io/mws-restaurant-stage-1/index.html',
+                'https://magicmart.github.io/mws-restaurant-stage-1/restaurant.html',
                 'https://magicmart.github.io/mws-restaurant-stage-1/css/styles.css',
                 'https://magicmart.github.io/mws-restaurant-stage-1/js/dbhelper.js',
                 'https://magicmart.github.io/mws-restaurant-stage-1/js/restaurant_info.js',
                 'https://magicmart.github.io/mws-restaurant-stage-1/js/main.js',
-                'https://magicmart.github.io/mws-restaurant-stage-1/data/restaurants.json'
+                'https://magicmart.github.io/mws-restaurant-stage-1/data/restaurants.json',
+                'https://magicmart.github.io/mws-restaurant-stage-1/img/1.jpg',
+                'https://magicmart.github.io/mws-restaurant-stage-1/img/2.jpg',
+                'https://magicmart.github.io/mws-restaurant-stage-1/img/3.jpg',
+                'https://magicmart.github.io/mws-restaurant-stage-1/img/4.jpg',
+                'https://magicmart.github.io/mws-restaurant-stage-1/img/5.jpg',
+                'https://magicmart.github.io/mws-restaurant-stage-1/img/6.jpg',
+                'https://magicmart.github.io/mws-restaurant-stage-1/img/7.jpg',
+                'https://magicmart.github.io/mws-restaurant-stage-1/img/8.jpg',
+                'https://magicmart.github.io/mws-restaurant-stage-1/img/9.jpg',
+                'https://magicmart.github.io/mws-restaurant-stage-1/img/10.jpg'
                 ]
             );
         })
